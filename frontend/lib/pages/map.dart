893013c14dbcbae158e5f774cae4c6a620f93975
Ignacio Ortiz <74736240+naxoku@users.noreply.github.com>
2023-10-14 06:51:38 -0300

    Obtener ubicación del usuario
    Se logra obtener la ubicación del usuario al presionar un botón

--- frontend/lib/pages/map.dart
+++ frontend/lib/pages/map.dart
@@ -1,23 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
-class Map extends StatelessWidget {
+
+class Map extends StatefulWidget {
   const Map({super.key});
 
   @override
+  State<Map> createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  // Coordenadas iniciales (Universidad)
+  late LatLng currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    currentLocation = LatLng(-33.471386, -71.2207616);
+  }
+
+  // Obtiene las coordenadas actuales
+   void getCurrentPosition() async {
+    Position position = await determinePosition();
+    updateLocation(LatLng(position.latitude, position.longitude));
+  }
+
+  // Actualiza las coordenadas en el mapa
+  void updateLocation(LatLng newLocation) {
+    setState(() {
+      currentLocation = newLocation;
+      print(currentLocation);
+    });
+  }
+
+  // Función que verifica si están los permisos para obtener la ubicación
+  Future<Position> determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+
+    // Si no tiene permiso, lo pide
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      // Si lo deniega, devuelve error
+      if (permission == LocationPermission.denied) {
+        return Future.error('Error');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-    options: MapOptions(
-      initialCenter: LatLng(-38.70288899111831, -72.54787751258495),
-      initialZoom: 16,
-    ),
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: currentLocation,
+          initialZoom: 6,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+          ElevatedButton(onPressed: () {
+            getCurrentPosition();
+          },
+          child: Text("Obtener ubicación"))
+        ],
       ),
-    ],
-  );
+    );
   }
 }
